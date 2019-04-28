9e0b72706c2129d8ae6fab9196764bec715afc89
Куприянов Павел <cbnhtyc@gmail.com>
2019-04-28 13:18:23 +0300

    добавление cors, удаление ненужного класса

--- fc-app/src/main/java/ru/kubsu/fs/configuration/FcAppConfig.java
+++ fc-app/src/main/java/ru/kubsu/fs/configuration/FcAppConfig.java
@@ -2,15 +2,29 @@ package ru.kubsu.fs.configuration;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.dozer.DozerBeanMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EntityScan("ru.kubsu.fs.*")
-public class FcAppConfig {
+public class FcAppConfig implements WebMvcConfigurer {
+
+    @Value("${cors.origin}")
+    private String[] origin;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+                .allowedOrigins(origin);
+    }
+
     @Bean
     public RestTemplate restTemplate() {
         RestTemplate restTemplate = new RestTemplate();
